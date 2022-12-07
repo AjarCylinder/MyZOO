1c4baca624160bed42767d6ccf3c406366360fd6
Кирилл Глумин <gluminkm@gmail.com>
2022-12-07 14:56:42 +0300

    add property to elephant

--- Zoo.py
+++ Zoo.py
@@ -1,34 +1,71 @@
 class Elephant:
 
     def __init__(self, name, food_day, age, whatEat):
-        self.name = name
+        self.__name = name
         self.food_day = food_day
-        self.age = age
+        self.__age = age
 
-        self.biom = "savannah"
-        self.place = "120 m^2"
-        self.whatEat = whatEat
-        self.predator = False
-        self.sound = "UUHHErrrrrrrrrrrRRRRRRRR"
+        self.__biom = "savannah"
+        self.__place = "120 m^2"
+        self.__whatEat = whatEat
+        self.__predator = False
+        self.__sound = "UUHHErrrrrrrrrrrRRRRRRRR"
         self.isFeeded = False
 
     def doSound(self):
-        print(self.name, ":", self.sound)
+        print(self.__name, ":", self.__sound)
 
     def eats(self, mass, typeOfEat):
-        if (typeOfEat in self.whatEat):
-            print(self.name, ": Ya pokushal")
+        if (typeOfEat in self.__whatEat):
+            print(self.__name, ": Ya pokushal")
             if mass >= self.food_day:
                 self.isFeeded = True
                 return self.isFeeded
             else:
                 return self.isFeeded
         else:
-            print(self.name, ": Ya ne budu eto est'")
+            print(self.__name, ": Ya ne budu eto est'")
 
     def doPlay(self):
-        print(self.name, ": Davai poigrayem!")
+        print(self.__name, ": Davai poigrayem!")
 
+    @property
+    def Age(self):
+        return self.__age
+
+    @Age.setter
+    def Age(self, value):
+        if value == int:
+            self.__age = value
+        else:
+            print("Одумайся, так нельзя")
+    @property
+    def Name(self):
+        return self.__name
+
+    @Name.setter
+    def Name(self, name):
+        self.__name = name
+
+    @property
+    def place(self):
+        return self.__place
+
+    @property
+    def biom(self):
+        return self.__biom
+
+    @property
+    def predator(self):
+        return self.__predator
+
+    @property
+    def sound(self):
+        return self.__sound
+
+    @property
+    def whatEat(self):
+        return self.__whatEat
 
 class Penguin:
     def __init__(self, name, food_day, age, whatEat):
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from Zoo import *
 
+egor = Elephant("Egor",5 , 15, ["сено"])
+
+
 
 
 
