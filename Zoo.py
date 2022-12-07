0d2cdf86a8004bebc20964400587b7c0ba529807
Кирилл Глумин <gluminkm@gmail.com>
2022-12-07 14:58:39 +0300

    add property to penguin

--- Zoo.py
+++ Zoo.py
@@ -69,15 +69,15 @@ class Elephant:
 
 class Penguin:
     def __init__(self, name, food_day, age, whatEat):
-        self.name = name
+        self.__name = name
         self.food_day = food_day
-        self.age = age
+        self.__age = age
 
-        self.biom = "tundra"
-        self.place = "16 m^2"
-        self.whatEat = whatEat
-        self.predator = True
-        self.sound = "Peeek"
+        self.__biom = "tundra"
+        self.__place = "16 m^2"
+        self.__whatEat = whatEat
+        self.__predator = True
+        self.__sound = "Peeek"
         self.isFeeded = False
 
     def doSound(self):
@@ -96,6 +96,45 @@ class Penguin:
 
     def doPlay(self):
         print(self.name, ": Davai poigrayem!")
+
+    @property
+    def Age(self):
+        return self.__age
+
+    @Age.setter
+    def Age(self, value):
+        if value == int:
+            self.__age = value
+        else:
+            print("Одумайся, так нельзя")
+
+    @property
+    def Name(self):
+        return self.__name
+
+    @Name.setter
+    def Name(self, name):
+        self.__name = name
+
+    @property
+    def place(self):
+        return self.__place
+
+    @property
+    def biom(self):
+        return self.__biom
+
+    @property
+    def predator(self):
+        return self.__predator
+
+    @property
+    def sound(self):
+        return self.__sound
+
+    @property
+    def whatEat(self):
+        return self.__whatEat
 
 
 class Tiger:
